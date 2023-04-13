ffad39979c9297d7b1707322c3b392bdf78525ce
MaanasMDK <mama9908@colorado.edu>
2023-04-13 12:16:09 -0600

    FP : Custom minimal build for RPI3b+ working

--- build.sh
+++ build.sh
@@ -9,7 +9,12 @@ git submodule update
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
 
-CONFLINE="MACHINE = \"qemuarm64\""
+################################################################
+# Description : Set hardware configuration for RPI 3b+
+# Author : Maanas Makam Dileep Kumar
+# Date : 04/13/23
+################################################################
+CONFLINE="MACHINE = \"raspberrypi3-64\""
 
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
@@ -22,6 +27,22 @@ else
 	echo "${CONFLINE} already exists in the local.conf file"
 fi
 
+################################################################
+# Description : Set the custom image output types
+# Author : Maanas Makam Dileep Kumar
+# Date : 04/13/23
+################################################################
+IMAGE="IMAGE_FSTYPES = \"tar.xz ext3 rpi-sdimg\""
+
+cat conf/local.conf | grep "${IMAGE}" > /dev/null
+local_image_info=$?
+
+if [ $local_image_info -ne 0 ];then 
+    echo "Append ${IMAGE} in the local.conf file"
+	echo ${IMAGE} >> conf/local.conf
+else
+	echo "${IMAGE} already exists in the local.conf file"
+fi
 
 bitbake-layers show-layers | grep "meta-aesd" > /dev/null
 layer_info=$?
@@ -31,6 +52,21 @@ if [ $layer_info -ne 0 ];then
 	bitbake-layers add-layer ../meta-aesd
 else
 	echo "meta-aesd layer already exists"
+fi
+
+################################################################
+# Description : Adding meta-raspberrypi layer.
+# Author : Maanas Makam Dileep Kumar
+# Date : 04/13/23
+################################################################
+bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
+layer_raspberrypi_info=$?
+
+if [ $layer_raspberrypi_info -ne 0 ];then
+	echo "Adding meta-raspberrypi layer"
+	bitbake-layers add-layer ../poky/meta-raspberrypi
+else
+	echo "meta-raspberrypi layer already exists"
 fi
 
 set -e
